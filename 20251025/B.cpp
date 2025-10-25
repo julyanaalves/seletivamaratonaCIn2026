592ff1e438f30cf0e54f887e82ddf526ad18ea1f
julyanaalves <jsa3@cin.ufpe.br>
2025-10-25 09:39:28 -0300

    questão B

--- 20251025/B.cpp
+++ 20251025/B.cpp
@@ -3,15 +3,26 @@ using namespace std;
 
 int main(){
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(nullptr); //optimize input/output
 
     int t;
     cin >> t;
+    if (!(cin >> t)) return 0; // Check for valid input
 
     while (t--){
-        int a, b;
+        int a, b, n = 0;
         cin >> a >> b;
-        
+
+        if (a == b){ // a == b
+            cout << 0 << "\n";
+            break;
+        } else if (a < b){
+            int d = b - a;
+            cout << (d % 2 == 1 ? 1 : 2) << "\n";
+        } else {
+            int d = a - b;
+            cout << (d % 2 == 0 ? 1 : 2) << "\n";
+        }
 
     }
     return 0;
