ab539a4fa3c9845d27fb830c0401b080a2c47c3a
Julyana Alves <jsa3@cin.ufpe.br>
2025-10-25 11:38:32 -0300

    F

---
+++ 20251025/F.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int t; if(!(cin >> t)) return 0;
+	while(t--){
+		int n; string s; 
+		cin >> n;
+		cin >> s; // length should be 4*n
+
+		int cntA = 0, cntB = 0, cntC = 0, cntD = 0;
+		for(char c : s){
+			if(c == 'A') ++cntA;
+			else if(c == 'B') ++cntB;
+			else if(c == 'C') ++cntC;
+			else if(c == 'D') ++cntD;
+		}
+		int ans = 0;
+		auto cap = [&](int x){ return x < n ? x : n; };
+		ans += cap(cntA);
+		ans += cap(cntB);
+		ans += cap(cntC);
+		ans += cap(cntD);
+
+		cout << ans << '\n';
+	}
+	return 0;
+}
+
